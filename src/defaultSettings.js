30093a40d73019a9abef2bd04a4a1a95080cda24
Li Zhang <lizhang@revosion.com>
2019-03-11 21:08:36 -0400

    first commit

---
+++ src/defaultSettings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "navTheme": "dark",
+  "primaryColor": "#1890FF",
+  "layout": "topmenu",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "autoHideHeader": false,
+  "fixSiderbar": false,
+  "menu": {
+    "disableLocal": false
+  },
+  "title": "中国泵云",
+  "pwa": true,
+  "iconfontUrl": "",
+  "collapse": true
+};
